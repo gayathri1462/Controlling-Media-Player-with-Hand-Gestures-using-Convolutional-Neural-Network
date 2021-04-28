d7e6270920b7ed365148df22c6cd3e0d874e3b4f
Gayathri Devi Nagalapuram <gayathri1462@gmail.com>
2021-04-28 22:53:03 +0530

    Update train.py

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ from keras.layers import Dense, Flatten
 model = Sequential()
 
 # First convolution layer and pooling
-model.add(Conv2D(32, (5, 5), activation='relu', input_shape=(120, 120, 1)))
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(120, 120, 1)))
 model.add(MaxPooling2D((2, 2)))
 
 # Second convolution layer and pooling
@@ -19,18 +19,16 @@ model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPooling2D((2, 2)))
 
 # Third convolution layer
-model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Conv2D(128, (3, 3), activation='relu'))
 
 # input_shape is going to be the pooled feature maps from the previous convolution layer
 model.add(MaxPooling2D((2, 2)))
 # Flattening the layers
 model.add(Flatten())
 
-
 # Adding a fully connected layer
-model.add(Dense(128, activation='relu'))
-model.add(Dense(7, activation='softmax')) # softmax for more than 2
-
+model.add(Dense(256, activation='relu'))
+model.add(Dense(7, activation='softmax'))
 # Compiling the CNN
 model.compile(optimizer='adam', # Optimization routine, which tells the computer how to adjust the parameter values to minimize the loss function.
               loss='categorical_crossentropy', # Loss function, which tells us how bad our predictions are.
@@ -47,25 +45,25 @@ train_datagen = ImageDataGenerator(
         rescale=1./255,
         shear_range=0.2,
         zoom_range=0.2,
-        horizontal_flip=True)
+        horizontal_flip= False)
 
 test_datagen = ImageDataGenerator(rescale=1./255)
 
 training_set = train_datagen.flow_from_directory('data/train',
                                                  target_size=(120, 120),
-                                                 batch_size=5,
+                                                 batch_size=7,
                                                  color_mode='grayscale',
                                                  class_mode='categorical')
 
 test_set = test_datagen.flow_from_directory('data/test',
                                             target_size=(120, 120),
-                                            batch_size=5,
+                                            batch_size=7,
                                             color_mode='grayscale',
                                             class_mode='categorical')
 model.fit(
         training_set,
         steps_per_epoch=125, # No of images in training set
-        epochs=10,
+        epochs=7,
         validation_data=test_set,
         validation_steps=50)# No of images in test set
 
